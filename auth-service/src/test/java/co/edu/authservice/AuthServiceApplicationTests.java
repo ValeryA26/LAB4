0ba0c20e0600f7b670139eeae477c5dac859970c
ValeryA26 <andy21211309@gmail.com>
2026-04-24 22:30:02 -0500

    parte 2 completada (puede que tenga errores)

--- auth-service/src/test/java/co/edu/authservice/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/co/edu/authservice/AuthServiceApplicationTests.java
@@ -1,13 +1,12 @@
 package co.edu.authservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class AuthServiceApplicationTests {
+@SpringBootApplication
+public class AuthServiceApplication {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
 	}
-
 }
