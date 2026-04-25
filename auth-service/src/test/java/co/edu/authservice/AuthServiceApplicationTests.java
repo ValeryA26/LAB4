303942750e3868b03e9ede58e796a342251a68c5
ValeryA26 <andy21211309@gmail.com>
2026-04-24 23:30:12 -0500

    parte 3 completada y probada

--- auth-service/src/test/java/co/edu/authservice/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/co/edu/authservice/AuthServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthServiceApplication {
+public class AuthServiceApplicationTests {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
